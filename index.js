c1166819424561cbabfb4756fd9d97dd9ef6b721
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-22 13:43:25 +0530

    added db connections

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-import express  from 'express'
+import express, { application }  from 'express'
 const app = express()
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
+import userRoutes from "./routes/user.js";
+import error from "./middleware/error.js";
 
 
+dotenv.config()
+
+mongoose.connect(process.env.DB_URL).then(res=>{
+  console.log("db connected successfully")
+})
+.catch(err=>{
+  console.log(err)
+})
 
 
-app.listen(5000,()=>{
-  console.log(`port start at ${5000}`)
+app.use(cors())
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/v1",userRoutes)
+app.use(error)
+
+app.listen(process.env.PORT,()=>{
+  console.log(`port start at ${process.env.PORT}`)
 })
