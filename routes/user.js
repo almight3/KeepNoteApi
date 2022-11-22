1d0ab15634ae48ccfa1251f292a060ee43e4c498
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-22 19:26:02 +0530

    refactor

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import express  from 'express'
 const router = express.Router();
 import { register,login,logout } from '../controller/userController.js';
+import {authenticateUser} from "../middleware/auth.js";
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/logout").get(logout);
+router.route("/logout").get(authenticateUser,logout);
 export default router
