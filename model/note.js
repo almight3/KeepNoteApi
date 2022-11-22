16f354243b15345460816c65746242e0e6b62781
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-22 19:24:50 +0530

    feat - added not schema

--- model/note.js
+++ model/note.js
@@ -1,3 +1,30 @@
 import mongoose from "mongoose";
-const {schema} = mongoose;
+const {Schema} = mongoose;
 
+const notesScheam = new Schema({
+    user:{
+       type:mongoose.Schema.ObjectId,
+       ref:"User",
+       required:true
+    },
+    title:{
+        type:String,
+        required:[true,"title is required"]
+    },
+    description:{
+        type:String,
+        required:[true,"discription is required"]
+    },
+    label:{
+        type:String
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+
+});
+
+const Notes = mongoose.model("Notes",notesScheam)
+
+export default Notes
