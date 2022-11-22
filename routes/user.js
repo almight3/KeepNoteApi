88d47d70709d317b2e3e9218cd270b565249c080
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-22 13:40:23 +0530

    feat - added routes for user

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 import express  from 'express'
 const router = express.Router();
+import { register,login,logout } from '../controller/userController.js';
 
-
-
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
 export default router
